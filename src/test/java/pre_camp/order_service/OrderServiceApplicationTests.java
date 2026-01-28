838298baf5821c8a8f661e49328b1325cc6abf5d
rlaxxwls13 <rlaxxwls13@hufs.ac.kr>
2026-01-28 16:02:05 +0900

    Feat: create spring project

---
+++ src/test/java/pre_camp/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pre_camp.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
